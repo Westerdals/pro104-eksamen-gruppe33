fab756c28963dee25abbbbcfd953a82537823437
crlsocro <55978835+crlsocro@users.noreply.github.com>
2020-05-22 17:25:28 +0200

    Update script.js
    kode på people

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ var btnPeople = document.getElementById("btn-people");
 
 var smallBtnWrapper = document.getElementsByClassName("small-btn-wrapper")[0];
 var formWrapper = document.getElementsByClassName("form-wrapper")[0];
+var groupWrapp = document.getElementsByClassName("group-wrapper")[0];
 
 //Initializing functions
 
 renderTaskList();
+renderGroup();
 
 // Assigning event listeners to buttons
 
@@ -32,9 +34,9 @@ function generateButtons(isTask) {
     } else {
         //btnPeople.style.color = "#165C73";
         //btnTask.style.color = "#F0C17C";
-        smallBtnWrapper.innerHTML = '<button class="btn-small" id="btn-new-group">NEW GROUP</button><button class="btn-small" id="btn-new-person">NEW PERSON</button>';
-        document.getElementById("btn-new-group").addEventListener("click", function() { groupForm("newList") });
-        document.getElementById("btn-new-person").addEventListener("click", function() { groupForm("newTask") });
+         smallBtnWrapper.innerHTML = '<button class="btn-small" id="btn-new-group">NEW GROUP</button><button class="btn-small" id="btn-new-person">NEW PERSON</button>';
+        document.getElementById("btn-new-group").addEventListener("click", function() { printForm("newGroup") });
+        document.getElementById("btn-new-person").addEventListener("click", function() { printForm("newPerson") });
     }
 }
 
@@ -49,19 +51,34 @@ function printForm(formType) {
 
             renderSelectListMenu();
         break;
-    }
-}
+            
+function printForm(formType) {
+    switch(formType) {
+        case "newList":
+            formWrapper.innerHTML = '<form onsubmit="createList(event)"><div class="form-input-field"><label for="list-name">LIST NAME:</label><input name="list-name-input" type="text"></div><div class="form-input-field"><label for="list-description">LIST DESCRIPTION:</label><input name="list-description-input" type="text"></div><div class="form-input-field"><button type="submit">ADD LIST</button></div></form>';
+        break;
 
-function groupForm(formtype){
-    switch(formtype){
+        case "newTask":
+            formWrapper.innerHTML = '<form onsubmit="createTask(event)"><div class="form-input-field"><label for="task-text">TASK:</label><input name="task-text" type="text"></div><div class="form-input-field"><div class="form-input-field"><label for="list-menu">LIST:</label><select name="list-menu" id="list-menu"></select></div><button type="submit">ADD TASK</button></div></form>';
+
+            renderSelectListMenu();
+        break;
         case "newGroup":
-            formWrapper.innerHTML = '<form onsubmit="createList(event)"><div class="form-input-field"><label for="groupName">Group Name:</label><input name="groupNameInput" type="text"></div><div class="form-input-field"><label for="Groupdescription">Group Description:</label><input name="Groupinput" type="text"></div><div class="form-input-field"><button type="submit">ADD GROUP</button></div></form>';
+            groupWrapp.innerHTML = '<form onsubmit="createGroupList(event)"><div class="form-input-field"><label for="groupNumber">Group Number:</label><input name="groupNumberInput" type="number"></div><button type="submit">ADD GROUP</button></div></form>';
         break;
 
         case "newPerson" :
-            formWrapper.innerHTML = '<form onsubmit="createList(event)"><div class="form-input-field"><label for="personName"> First Name:</label><input name="personNameInput" type="text"></div><div class="form-input-field"><label for="GroupMenu">Select group:</label><select name="Groupmenu" id="Groupmenu"></select></div><button type="submit">ADD PERSON</button></div></form>';
-        }
+            groupWrapp.innerHTML = '<form onsubmit="createPerson(event)"><div class="form-input-field"><label for="personName"> First Name:</label><input name="personNameInput" type="text"></div><div class="form-input-field"><label for="groupMenu">Select group:</label><select name="groupMenu" id="groupMenu"></select></div><button type="submit">ADD PERSON TO GROUP</button></div></form>';
+            
+            renderSelectGroups();
+            
+        break;
+    }
 }
+    }
+}
+
+
 
 function renderTaskList() {
 
@@ -118,7 +135,63 @@ function createTask(event) {
     const taskArray = JSON.parse(window.localStorage.getItem(taskArray)) || [];
     taskArray.push(taskObj);
     window.localStorage.setItem("taskArray", JSON.stringify(taskArray)); 
-
-
 }
+
+function renderGroup(){
+    const groupArray  = JSON.parse(window.localStorage.getItem("groupArray"))  || [];
+    const groupS = document.getElementById("Group-container");
+    groupS.innerHTML = "";
+
+    for(const groupObj of groupArray){
+        const groupElement = document.createElement("div");
+        groupElement.className = "groupDiv";
+        groupElement.innerText = `${groupObj.groupNumber} + ${groupObj.personName}`;
+        groupS.appendChild(groupElement);
+    }
+}
+
+function renderSelectGroups(){
+    groupMenu = document.getElementById("groupMenu");
+    const groupArray = JSON.parse(window.localStorage.getItem("groupArray")) || [];
+    
+    groupMenu.innerHTML = "";
+    for(const groupObj of groupArray){
+        groupMenu.innerHTML += `<option id="${groupObj.groupName}">${groupObj.groupName}</option>`;
+    }
+}
+
+function createGroupList(event){
+    event.preventDefault();
+
+    const groupNumber = document.querySelector("[name = 'groupNumberInput']").value;
+    
+    const groupObj = {groupNumber};
+
+    console.log(groupObj);
+
+    const groupArray = JSON.parse(window.localStorage.getItem(groupArray)) || [];
+    groupArray.push(groupObj);
+    window.localStorage.setItem("groupArray", JSON.stringify(groupArray));
+
+    renderSelectGroups();
+    groupWrapp.innerHTML = "";
+}
+
+function createPerson(event){
+    event.preventDefault();
+
+    const firstName = document.querySelector("[name = 'personName']").value;
+    const assingToGroup = document.querySelector("[name = 'groupMenu']").value;
+
+    const personObj ={firstName, assingToGroup};
+
+    const personArray = JSON.parse(window.localStorage.getItem(personArray)) || [];
+    personArray.push(personObj);
+
+    window.localStorage.setItem("personArray", JSON.stringify(personArray));
+
+    renderSelectGroups();
+    groupWrapp.innerHTML = "";
+}
+
 
